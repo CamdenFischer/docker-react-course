c9de351069e9ab583fd33c78d1bf7dec9f313096
Camden Fischer <camdenfischer@gmail.com>
2023-01-30 11:29:44 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          What the fuck is up Kyle!?
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/What the fuck is up Kyle!?/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/What the fuck is up Kyle!?/i);
   expect(linkElement).toBeInTheDocument();
 });
